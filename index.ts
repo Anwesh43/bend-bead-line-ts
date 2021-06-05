14a6d910cc43bedad0bc20b3fddf29e856890278
Anwesh43 <anweshthecool0@gmail.com>
2021-06-06 04:32:33 +0530

    adding Renderer class

--- index.ts
+++ index.ts
@@ -218,3 +218,25 @@ class BendBeadLine {
         this.curr.startUpdating(cb)
     }
 }
+
+class Renderer {
+
+    curr : BendBeadLine = new BendBeadLine()
+    animator : Animator = new Animator()
+
+    render(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    handleTap(cb : Function) {
+        this.curr.startUpdating(() => {
+            this.animator.start(() => {
+                cb()
+                this.curr.update(() => {
+                    this.animator.stop()
+                    cb()
+                })
+            })
+        })
+    }
+}
